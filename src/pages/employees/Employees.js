65bc5afc144bf60153ec7ab32283a8b4ab5b0d8d
EAM26 <emuller2607@gmail.com>
2023-11-20 13:35:26 +0100

    Add getEmployees request

--- src/pages/employees/Employees.js
+++ src/pages/employees/Employees.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function Employees() {
+
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        async function getEmployees() {
+            try {
+                const response = await axios.get('http://localhost:8080/employees')
+                setEmployees(response.data)
+                console.log(employees)
+            } catch (e) {
+                console.error("Kon employees niet ophalen: ", e)
+            }
+
+        }
+
+        getEmployees();
+
+    }, []);
     return (
         <div>
             <h2>Employees</h2>
@@ -17,27 +36,17 @@ function Employees() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>Test table data</td>
-                    <td>Test table data</td>
-                    <td>Test table data</td>
-                    <td>Test table data</td>
-                    <td>Test table data</td>
-                    <td>Test table data</td>
-                </tr>
-                {/*{employees.map((employee) => {*/}
-                {/*        return <tr key={employee.id}>*/}
-                {/*            <td>{employee.id}</td>*/}
-                {/*            <td>{employee.shortName}</td>*/}
-                {/*            <td>{employee.firstName}</td>*/}
-                {/*            <td>{employee.preposition} {employee.lastName}</td>*/}
-                {/*            <td>{employee.email}</td>*/}
-                {/*            <td>{employee.team.teamName}</td>*/}
-
-                {/*        </tr>*/}
-
-                {/*    }*/}
-                {/*)}*/}
+                {employees.map((employee) => {
+                        return <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.shortName}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.preposition} {employee.lastName}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.team.teamName}</td>
+                        </tr>
+                    }
+                )}
                 </tbody>
             </table>
         </div>
