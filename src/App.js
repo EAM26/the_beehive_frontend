2a1b9a5597fa9ed5259e6ce31c8cebd8f454a2f7
EAM26 <emuller2607@gmail.com>
2023-11-20 13:02:39 +0100

    Setup routing to Employees

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Employees from "./pages/employees/Employees";
 
 function App() {
-  return (
-    <>
-      <Navbar/>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+            <Route path="/employees" element={<Employees/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,19 +3,24 @@ import '../../App.css';
 import './Navbar.css';
 import Button from "../button/Button";
 import beehiveLogo from '../../assets/beehive.svg';
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="navbar-container">
-            <div className="logo-container"><img className="nav-img" src={beehiveLogo} alt="beehive-logo"/>
-                <h2>Beehive</h2></div>
+        <>
+            <nav className="navbar-container">
+                <div className="logo-container"><img className="nav-img" src={beehiveLogo} alt="beehive-logo"/>
+                    <h2>Beehive</h2></div>
 
-            <ul className="navbar-ul">
-                <li><a href="/">Personeel</a></li>
-                <li><a href="/">Roosters</a></li>
-                <li><Button type="button" text="Login" className="btn" children="Login"/></li>
-            </ul>
-        </nav>
+                <ul className="navbar-ul">
+                    <NavLink to="/employees">
+                        <div>Personeel</div>
+                    </NavLink>
+                    <li><a href="/">Roosters</a></li>
+                    <li><Button type="button" text="Login" className="btn" children="Login"/></li>
+                </ul>
+            </nav>
+        </>
     );
 }
 
