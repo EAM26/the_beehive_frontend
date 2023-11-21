306cf93a20c2c6d9e7f67098e1edbc0dcad63494
EAM26 <emuller2607@gmail.com>
2023-11-21 13:14:24 +0100

    Add functions to AuthContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Employees from "./pages/employees/Employees";
 import Login from "./pages/login/Login";
+import {useContext} from "react";
+import {AuthContext} from "./context/AuthContext";
 
 function App() {
+     const { isAuth } = useContext(AuthContext)
     return (
         <>
             <Navbar/>
             <Routes>
-            <Route path="/employees" element={<Employees/>}/>
-            <Route path="/login" element={<Login/>}/>
+                <Route path="/employees" element={ isAuth ? <Employees/> : <Navigate to={'/login'} /> }/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </>
     );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,7 @@
 import React, {createContext, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import {jwtDecode} from 'jwt-decode';
+import axios from "axios";
 
 
 export const AuthContext = createContext({});
@@ -11,28 +13,73 @@ function AuthContextProvider({children}) {
         user: null,
         status: 'pending'});
 
-    const {navigate} = useNavigate();
+    const navigate  = useNavigate();
 
     useEffect(() => {
         const storedToken = localStorage.getItem('token')
         if(storedToken) {
             login(storedToken)
+        } else {
+            logout()
         }
+
     }, []);
 
-    function login() {
-        console.log("login function ")
+    async function fetchUserData(jwt, redirect) {
+        try {
+            const response = await axios.get('http://localhost:8080/authenticated', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${jwt}`
+                }
+            })
+            setAuthState( {
+                ...authState,
+                isAuth: true,
+                user: {
+                    username: response.data.username,
+                    authorities: response.data.authorities
+                },
+                status: "done",
+
+            })
+            console.log("function fetchUserData activated: ", response.data)
+            if(redirect) navigate(redirect)
+        } catch (e) {
+            console.error("Couldn't fetch userdata: ", e)
+        }
+    }
+    function login(jwt, redirect) {
+        console.log("login function activated")
+        const decodedToken = jwtDecode(jwt)
+        console.log("decodedToken: ", decodedToken)
+        localStorage.setItem('token', jwt)
+        fetchUserData(jwt, redirect)
     }
 
     function logout() {
         console.log("logout function")
+        localStorage.removeItem('token')
+        setAuthState( {
+            ...authState,
+            isAuth: false,
+            user: null,
+            status: "done",
+        })
+        navigate('/login')
     }
 
     const contextData = {
+        isAuth: authState.isAuth,
+        user: authState.user,
+        login: login,
+        logout: logout,
+    };
 
-    }
+
     return (
         <AuthContext.Provider value={contextData}>
+            { authState.status === "done" ? children : <p>Loading...</p>}
             {children}
         </AuthContext.Provider>
     );
