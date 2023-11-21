ee1cc5b8ec08026a6130a21e16f49251036c4bd1
EAM26 <emuller2607@gmail.com>
2023-11-21 15:33:01 +0100

    Add FormInputField to Login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import Button from "../../components/button/Button";
 import './Login.css';
+import FormInputField from "../../components/FormInputField/FormInputField";
+
 
 function Login() {
 
@@ -11,33 +13,29 @@ function Login() {
 
         console.log("function handleForm activated: ", data)
     }
+
     return (
         <div className="outer-container">
             <div className="inner-container">
                 <form className="form-outer-container"
-                    onSubmit={handleSubmit(handleFormSubmit)}>
-                    <label
-                        htmlFor="username">
-                        Gebruikersnaam:
-                        <input
-                            type="text"
-                            id="username"
-                            placeholder="Typ hier je gebruikersnaam"
-                            {...register("username")}
-                        />
-                    </label>
-                    <label
-                        htmlFor="password">
-                        Wachtwoord:
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Typ hier je wachtwoord"
-                            {...register("password")}
-                        />
-                    </label>
-
-                    <Button type="submit" children="Submit"/>
+                      onSubmit={handleSubmit(handleFormSubmit)}>
+                    <FormInputField
+                        label="Gebruikersnaam:"
+                        name="username"
+                        type="text"
+                        id="username"
+                        placeholder="Typ hier je gebruikersnaam"
+                        register={register}
+                    />
+                    <FormInputField
+                        label="Wachtwoord:"
+                        name="password"
+                        type="password"
+                        id="password"
+                        placeholder="Typ hier je wachtwoord"
+                        register={register}
+                    />
+                    <Button type="submit" children="Inloggen"/>
                 </form>
             </div>
         </div>
