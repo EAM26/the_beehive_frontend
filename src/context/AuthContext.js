b9ebed631470973c05a0edffd177d0f021ae82db
EAM26 <emuller2607@gmail.com>
2023-11-30 14:24:26 +0100

    Create seperate request getUserData in service

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,35 +1,31 @@
 import React, {createContext, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
-import { checkTokenValidity } from "../helpers/checkTokenValidity";
+import {checkTokenValidity} from "../helpers/checkTokenValidity";
 import {getHighestRole} from "../helpers/getHighestRole";
-
+import {errorHandler} from "../helpers/errorHandler";
+import {getUserData} from "../service";
 
 
 export const AuthContext = createContext({});
 
 function AuthContextProvider({children}) {
 
-    // function hasAdminOrManagerRole(authorities) {
-    //     console.log("How authorities looks: ", authorities)
-    //     console.log(authorities[0])
-    //     return authorities.some(auth =>
-    //         auth.authority === 'ROLE_ADMIN' || auth.authority === 'ROLE_MANAGER'
-    //     );
-    // }
-
     const [authLevel, setAuthLevel] = useState('');
     const [authState, setAuthState] = useState({
         isAuth: false,
         user: null,
-        status: 'pending'});
+        status: 'pending'
+    });
 
-    const navigate  = useNavigate();
+    const navigate = useNavigate();
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrormessage] = useState("")
 
 
     useEffect(() => {
         const storedToken = localStorage.getItem('token')
-        if(storedToken && checkTokenValidity(storedToken)) {
+        if (storedToken && checkTokenValidity(storedToken)) {
             login(storedToken)
         } else {
             logout()
@@ -37,15 +33,11 @@ function AuthContextProvider({children}) {
 
     }, []);
 
-    async function fetchUserData(jwt, redirect) {
+    async function fetchData(jwt, redirect) {
         try {
-            const {data: { principal, authorities } } = await axios.get('http://localhost:8080/authenticated', {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${jwt}`
-                }
-            })
-            setAuthState( {
+
+            const { principal, authorities} = await getUserData(jwt);
+            setAuthState({
                 ...authState,
                 isAuth: true,
                 user: {
@@ -53,25 +45,33 @@ function AuthContextProvider({children}) {
                     authorities: authorities
 
                 },
-                status: "done",
+                status: "done"
             })
-            console.log("how authorities looks: ", authorities)
             setAuthLevel(getHighestRole(authorities))
 
-            if(redirect) navigate(redirect)
         } catch (e) {
-            console.error("Couldn't fetch userdata: ", e)
+            setError(true)
+            setErrormessage(errorHandler(e))
+            setAuthState({
+                ...authState,
+                status: 'done'
+            })
+
         }
+        finally {
+            if (redirect) navigate(redirect)
+        }
+
     }
+
     function login(jwt, redirect) {
         localStorage.setItem('token', jwt)
-         void fetchUserData(jwt, redirect)
-
+        void fetchData(jwt, redirect)
     }
 
     function logout() {
         localStorage.removeItem('token')
-        setAuthState( {
+        setAuthState({
             ...authState,
             isAuth: false,
             user: null,
@@ -80,18 +80,22 @@ function AuthContextProvider({children}) {
         navigate('/login')
     }
 
-    const contextData = {
+    const authContextData = {
         isAuth: authState.isAuth,
         user: authState.user,
-        login: login,
-        logout: logout,
-        authLevel: authLevel,
+        login,
+        logout,
+        authLevel,
+        error,
+        setError,
+        errorMessage,
+        setErrormessage,
     };
 
 
     return (
-        <AuthContext.Provider value={contextData}>
-            { authState.status === "done" ? children : <p>Loading...</p>}
+        <AuthContext.Provider value={authContextData}>
+            {authState.status === "done" ? children : <p>Loading...</p>}
         </AuthContext.Provider>
     );
 }
--- src/service.js
+++ src/service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 
+//Request to get all employees
 export const getEmployees = async (token, signal) => {
 
     const response = await axios.get('http://localhost:8080/employees',
@@ -15,10 +16,23 @@ export const getEmployees = async (token, signal) => {
 
 }
 
+// Request for login
 export const postLoginData = async(data) => {
     const response = await axios.post(`http://localhost:8080/authenticate`, {
         username: data.username,
         password: data.password,
     })
     return response.data;
+}
+
+//Request to get user data
+export const getUserData = async (jwt) => {
+
+    const response = await axios.get('http://localhost:8080/authenticated', {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${jwt}`
+        }
+    })
+    return response.data
 }
