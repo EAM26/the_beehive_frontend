9e928c4248de5079176ecb181310e74a0d3c60c1
EAM26 <emuller2607@gmail.com>
2024-02-03 20:38:09 +0100

    Add LocaleContext for date formatting to local standard

--- src/App.js
+++ src/App.js
@@ -5,25 +5,30 @@ import Employees from "./pages/employees/Employees";
 import Login from "./pages/login/Login";
 import {useContext,} from "react";
 import {AuthContext} from "./context/AuthContext";
+import LocaleContextProvider from "./context/LocaleContext";
 import TempHome from "./pages/tempPages/TempHome";
 import TempRosters from "./pages/tempPages/TempRosters";
 import Profile from "./pages/profiles/Profile";
 import Users from "./pages/users/Users";
 
 function App() {
-    const {isAuth, authLevel } = useContext(AuthContext)
+    const {isAuth, authLevel} = useContext(AuthContext)
     return (
         <>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={isAuth ? <TempHome/> : <Login/>}/>
-                <Route path="/login" element={!isAuth ? <Login/> : <TempHome/>}/>
-                <Route path="/profile/:id" element={isAuth ? <Profile/> : <Navigate to={'/login'}/>}/>
-                <Route path="/profile" element={isAuth ? <Profile/> : <Navigate to={'/login'}/>}/>
-                <Route path="/employees" element={isAuth && (authLevel !== 'user') ? <Employees/> : <Navigate to={'/'}/>}/>
-                <Route path="/users" element={isAuth && (authLevel !== 'user') ? <Users/> : <Navigate to={'/'}/>}/>
-                <Route path="/rosters" element={isAuth && (authLevel !== 'user') ? <TempRosters/> : <Navigate to={'/'}/>}/>
-            </Routes>
+            <LocaleContextProvider>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={isAuth ? <TempHome/> : <Login/>}/>
+                    <Route path="/login" element={!isAuth ? <Login/> : <TempHome/>}/>
+                    <Route path="/profile/:id" element={isAuth ? <Profile/> : <Navigate to={'/login'}/>}/>
+                    <Route path="/profile" element={isAuth ? <Profile/> : <Navigate to={'/login'}/>}/>
+                    <Route path="/employees"
+                           element={isAuth && (authLevel !== 'user') ? <Employees/> : <Navigate to={'/'}/>}/>
+                    <Route path="/users" element={isAuth && (authLevel !== 'user') ? <Users/> : <Navigate to={'/'}/>}/>
+                    <Route path="/rosters"
+                           element={isAuth && (authLevel !== 'user') ? <TempRosters/> : <Navigate to={'/'}/>}/>
+                </Routes>
+            </LocaleContextProvider>
         </>
     );
 }
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {getSelf} from "../../service";
 import FormInputField from "../../components/FormInputField/FormInputField";
 import {useForm} from "react-hook-form";
 import Button from "../../components/button/Button";
 import Checkbox from "../../components/checkbox/Checkbox";
 import {errorHandler} from "../../helpers/errorHandler";
+import {LocaleContext} from "../../context/LocaleContext";
 import "./Profile.css"
 
 function Profile() {
@@ -15,13 +16,14 @@ function Profile() {
     const [errorMessage, setErrormessage] = useState("")
     const [isDeleted, setIsDeleted] = useState(false);
     const [isActive, setIsActive] = useState(false);
-    const [shifts, setShifts] = useState(null)
+    const userLocale = useContext(LocaleContext)
 
     useEffect(() => {
         const token = localStorage.getItem('token');
 
         const fetchSelfData = async () => {
             try {
+                setLoading(true);
                 const selfData = await getSelf(token);
                 setSelf(selfData);
             } catch (e) {
@@ -74,171 +76,160 @@ function Profile() {
     return (
 
         <main>
-            <form
-                onSubmit={handleSubmit(handleFormSubmitUser)}>
+            {error ? <p>{errorMessage}</p> :
+                <div>
+                    <form
+                        onSubmit={handleSubmit(handleFormSubmitUser)}>
 
-                {/*USER DATA*/}
-                <FormInputField
-                    label="User name"
-                    name="username"
-                    type="text"
-                    id="username"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self ? self.username : ""}
-                />
-                <FormInputField
-                    label="Email"
-                    name="email"
-                    type="email"
-                    id="email"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self ? self.email : ""}
-                />
-                <Checkbox
-                    label="User Not Deleted"
-                    name="isDeleted"
-                    checked={!isDeleted}
-                    onChange={handleIsDeletedChange}
-                />
+                        {/*USER DATA*/}
+                        <FormInputField
+                            label="User name"
+                            name="username"
+                            type="text"
+                            id="username"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self ? self.username : ""}
+                        />
+                        <FormInputField
+                            label="Email"
+                            name="email"
+                            type="email"
+                            id="email"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self ? self.email : ""}
+                        />
+                        <Checkbox
+                            label="User Not Deleted"
+                            name="isDeleted"
+                            checked={!isDeleted}
+                            onChange={handleIsDeletedChange}
+                        />
 
-                {/*Employee Data*/}
-                <FormInputField
-                    label="Employee id"
-                    name="empId"
-                    type="text"
-                    id="empId"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.id : ""}
-                />
-                <Checkbox
-                    label="Employee Active"
-                    name="isActive"
-                    checked={isActive}
-                    onChange={handleIsActiveChange}
-                />
-                <FormInputField
-                    label="First Name"
-                    name="firstName"
-                    type="text"
-                    id="firstName"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.firstName : ""}
-                />
-                <FormInputField
-                    label="Preposition"
-                    name="preposition"
-                    type="text"
-                    id="preposition"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.preposition : ""}
-                />
-                <FormInputField
-                    label="Last name"
-                    name="lastName"
-                    type="text"
-                    id="lastName"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.lastName : ""}
-                />
-                <FormInputField
-                    label="Short name"
-                    name="shortName"
-                    type="text"
-                    id="shortName"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.shortName : ""}
-                />
-                <FormInputField
-                    label="Date of Birth"
-                    name="dob"
-                    type="date"
-                    id="dob"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.dob : ""}
-                />
-                <FormInputField
-                    label="Phone number"
-                    name="phoneNumber"
-                    type="text"
-                    id="phoneNumber"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.phoneNumber : ""}
-                />
-                <FormInputField
-                    label="Employee id"
-                    name="empId"
-                    type="text"
-                    id="empId"
-                    register={register}
-                    errors={errors}
-                    defaultValue={self.employee ? self.employee.id : ""}
-                />
-                <Button type="submit" children="Opslaan"/>
-            </form>
+                        {/*Employee Data*/}
+                        <FormInputField
+                            label="Employee id"
+                            name="empId"
+                            type="text"
+                            id="empId"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.id : ""}
+                        />
+                        <Checkbox
+                            label="Employee Active"
+                            name="isActive"
+                            checked={isActive}
+                            onChange={handleIsActiveChange}
+                        />
+                        <FormInputField
+                            label="First Name"
+                            name="firstName"
+                            type="text"
+                            id="firstName"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.firstName : ""}
+                        />
+                        <FormInputField
+                            label="Preposition"
+                            name="preposition"
+                            type="text"
+                            id="preposition"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.preposition : ""}
+                        />
+                        <FormInputField
+                            label="Last name"
+                            name="lastName"
+                            type="text"
+                            id="lastName"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.lastName : ""}
+                        />
+                        <FormInputField
+                            label="Short name"
+                            name="shortName"
+                            type="text"
+                            id="shortName"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.shortName : ""}
+                        />
+                        <FormInputField
+                            label="Date of Birth"
+                            name="dob"
+                            type="date"
+                            id="dob"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.dob : ""}
+                        />
+                        <FormInputField
+                            label="Phone number"
+                            name="phoneNumber"
+                            type="text"
+                            id="phoneNumber"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.phoneNumber : ""}
+                        />
+                        <FormInputField
+                            label="Employee id"
+                            name="empId"
+                            type="text"
+                            id="empId"
+                            register={register}
+                            errors={errors}
+                            defaultValue={self.employee ? self.employee.id : ""}
+                        />
+                        <Button type="submit" children="Opslaan"/>
+                    </form>
 
-            <div>
-                SHIFTS
-                <div className="shifts-container">
-                    {self.shifts ? self.shifts.slice(0, 5).map((shift) => {
-                        // Create Date objects from the startShift and endShift strings
-                        const startShiftDate = new Date(shift.startShift);
-                        const endShiftDate = new Date(shift.endShift);
+                    <div>
+                        SHIFTS
+                        <div className="shifts-container">
+                            {self.shifts ? self.shifts.slice(0, 5).map((shift) => {
 
-                        // Get the user's locale from the browser
-                        const userLocale = navigator.language;
-
-                        // Format the dates and times
-                        const date = `${startShiftDate.getDate().toString().padStart(2, '0')}-${(startShiftDate.getMonth() + 1).toString().padStart(2, '0')}-${startShiftDate.getFullYear()}`;
-                        // const date = startShiftDate.toLocaleDateString(userLocale) // Local format for day-month-year
-                        const startTime = startShiftDate.toLocaleTimeString(userLocale, {
-                            hour: '2-digit',
-                            minute: '2-digit'
-                        });
-                        const endTime = endShiftDate.toLocaleTimeString(userLocale, {
-                            hour: '2-digit',
-                            minute: '2-digit'
-                        });
-
-                        // Return the formatted string
-                        return <p key={shift.id}>{date}  {startTime}  {endTime}</p>
-                    }) : "No Shifts Available"
-                    }
-                </div>
-            </div>
-            <div>
-                ABSENCES
-                <div className="absences-container">
-                    {self.absences ? self.absences.slice(0, 5).map((absence) => {
-                        // Create Date objects from the startShift and endShift strings
-                        const startAbsenceDate = new Date(absence.startDate);
-                        const endAbsenceDate = new Date(absence.endDate);
-
-                        // Get the user's locale from the browser
-                        const userLocale = navigator.language;
+                                const startShiftDate = new Date(shift.startShift);
+                                const endShiftDate = new Date(shift.endShift);
 
 
-                        const startDate = `${startAbsenceDate.getDate().toString().padStart(2, '0')}-${(startAbsenceDate.getMonth() + 1).toString().padStart(2, '0')}-${startAbsenceDate.getFullYear()}`;
-                        const endDate = `${endAbsenceDate.getDate().toString().padStart(2, '0')}-${(endAbsenceDate.getMonth() + 1).toString().padStart(2, '0')}-${endAbsenceDate.getFullYear()}`;
+                                const date = `${startShiftDate.getDate().toString().padStart(2, '0')}-${(startShiftDate.getMonth() + 1).toString().padStart(2, '0')}-${startShiftDate.getFullYear()}`;
+                                const startTime = startShiftDate.toLocaleTimeString(userLocale, {
+                                    hour: '2-digit',
+                                    minute: '2-digit'
+                                });
+                                const endTime = endShiftDate.toLocaleTimeString(userLocale, {
+                                    hour: '2-digit',
+                                    minute: '2-digit'
+                                });
 
+                                // Return the formatted string
+                                return <p key={shift.id}>{date} {startTime} - {endTime}</p>
+                            }) : "No Shifts Available"}
+                        </div>
+                    </div>
+                    <div>
+                        ABSENCES
+                        <div className="absences-container">
+                            {self.absences ? self.absences.slice(0, 5).map((absence) => {
 
+                                const startAbsenceDate = new Date(absence.startDate);
+                                const endAbsenceDate = new Date(absence.endDate);
 
+                                const startDate = `${startAbsenceDate.getDate().toString().padStart(2, '0')}-${(startAbsenceDate.getMonth() + 1).toString().padStart(2, '0')}-${startAbsenceDate.getFullYear()}`;
+                                const endDate = `${endAbsenceDate.getDate().toString().padStart(2, '0')}-${(endAbsenceDate.getMonth() + 1).toString().padStart(2, '0')}-${endAbsenceDate.getFullYear()}`;
 
-
-                        return <p key={absence.id}>{startDate}  {endDate}</p>
-                    }) : "No Absences Available"
-                    }
-                </div>
-            </div>
-
+                                return <p key={absence.id}>{startDate} {endDate}</p>
+                            }) : "No Absences Available"
+                            }
+                        </div>
+                    </div>
+                </div>}
         </main>
 
     );
