674aa7286928f9ae0fe7147bce28f726838087c7
EAM26 <emuller2607@gmail.com>
2024-02-05 12:29:57 +0100

    Remove redundant code made obsolete by new profile page implementation

--- src/service.js
+++ src/service.js
@@ -1,21 +1,6 @@
 import axios from "axios";
 
 
-//Request to get all employees
-export const getEmployees = async (token, signal) => {
-
-    const response = await axios.get('http://localhost:8080/employees',
-        {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            signal: signal
-        })
-
-    return response.data
-
-}
 
 // Request to get all Users data with employee data
 export const getUsers = async (token, signal) => {
@@ -68,16 +53,7 @@ export const getAuthData = async (jwt) => {
     return response.data
 }
 
-export const getSingleEmployeeData = async (jwt, id) => {
-    console.log("getSingleEmployeeData running")
-    const response = await axios.get(`http://localhost:8080/employees/profile/${id}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${jwt}`
-        }
-    })
-    return response.data
-}
+
 
 export const getUserData = async (jwt, username)=> {
     // const authData  = await getAuthData(jwt)
@@ -90,18 +66,7 @@ export const getUserData = async (jwt, username)=> {
     return response.data
 }
 
-// Request to get own profile data
-// export const getProfileData = async (token)=> {
-//
-//     const response = await axios.get('localhost:8080/users/self', {
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: `Bearer ${jwt}`
-//         }
-//     })
-//     console.log(response.data)
-//     return response.data
-// }
+
 
 
 
