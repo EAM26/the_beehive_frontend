fd78484423f8d9e6f934af0319387bed74189e46
EAM26 <emuller2607@gmail.com>
2024-04-02 17:32:06 +0200

    Add content to NotFound.js

--- src/pages/not found/NotFound.js
+++ src/pages/not found/NotFound.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './NotFound.css'
+import {useNavigate} from "react-router-dom";
+import chilling_bee from '../../assets/chilling_bee.png'
 
 function NotFound(props) {
+
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        navigate('/');
+    }, 3000);
+
+    const [counter, setCounter] = useState(3);
+
+    setInterval(() => {
+        setCounter(counter - 1);
+    }, 1000);
+
+
     return (
-        <div></div>
+        <div className="outer-container">
+            <div className="inner-container">
+                <div className="not-found-page">
+                    <span>404</span>
+                    <img src={chilling_bee} alt="chilling bee image"/>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
